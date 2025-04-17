225ab9a27661b09b9c0a6007041ee46fd857e436
Poramed Thienkriengkrai <poramed.thienkriengkrai@apar.com>
2025-04-17 15:05:52 +0700

    feat: :sparkles: init project

---
+++ app/api/send-discord/route.ts
@@ -0,0 +1,12 @@
+'use server'
+
+export const sendToDiscord = async(message: string) => {
+    const webhookURL = `https://discord.com/api/webhooks/${process.env.NEXT_PUBLIC_DISCORD_ID}/${process.env.NEXT_PUBLIC_DISCORD_TOKEN}`
+
+    await fetch(webhookURL,{
+        method:'POST',
+        headers:{ 'Content-Type': 'application/json'},
+        body: JSON.stringify({content: message})
+    }
+    )
+}
